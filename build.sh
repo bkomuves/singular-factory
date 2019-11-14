6a0d731222e11227724ad809e20eaa8eb879346a
Balazs Komuves <bkomuves@gmail.com>
2019-11-14 23:05:58 +0100

    cabalize

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 rm *.o
 rm test
 
-CFLAGS=`pkg-config --cflags-only-I singular`
-LIBS=`pkg-config --libs singular`
+CFLAGS=`pkg-config --cflags-only-I factory`
+LIBS=`pkg-config --libs factory`
 
 g++ -c -m64 -x c++ -fpic $CFLAGS ./cpp_bits/interface.cpp -o interface.o 
 ghc --make -O main.hs interface.o -o main ${LIBS} -lstdc++
